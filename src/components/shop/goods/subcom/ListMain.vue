a9050bf3a684410ab93024e499b8fb4984bbe9ae
夜幕暮夜 <a1664187734@163.com>
2018-03-06 22:25:11 +0800

    商品详情的主页下部分

--- src/components/shop/goods/subcom/ListMain.vue
+++ src/components/shop/goods/subcom/ListMain.vue
@@ -1,49 +1,42 @@
 <template>
- <div class="section">
+  <div class="ListMain">
+    <div class="section" v-for="item in dates" :key="item.level1cateid" >
+    <!--子类-->
+    <div class="main-tit" >
+      <h2>{{item.catetitle}}</h2>
+      <p>
+        <a href="#"  v-for="value in item.level2catelist" :key="value.subcateid">
+          {{value.subcatetitle}}
+        </a>
+      </p>
+    </div>
+    <!--/子类-->
+    <div class="wrapper clearfix">
+      <div class="wrap-box">
+        <ul class="img-list">
 
-  <!--子类-->
-  <div class="main-tit">
-    <h2>手机数码</h2>
-    <p>
+          <li v-for="index in item.datas" :key="index.artID">
+           <router-link :to="{name:'detail',params:{id:index.artID}}">
+              <div class="img-box">
+                <img :src="index.img_url">
+              </div>
+              <div class="info">
+                <h3>{{index.artTitle}}</h3>
+                <p class="price">
+                  <b>¥{{index.market_price}}</b>元</p>
+                <p>
+                  <strong>库存 {{index.stock_quantity}}</strong>
+                  <span>市场价：
+                    <s>{{index.sell_price}}</s>
+                  </span>
+                </p>
+              </div>
 
-      <a href="/goods/43.html">手机通讯</a>
+            </router-link>
+          </li>
 
-      <a href="/goods/44.html">摄影摄像</a>
-
-      <a href="/goods/45.html">存储设备</a>
-
-      <a href="/goods/40.html">更多
-        <i>+</i>
-      </a>
-    </p>
-  </div>
-  <!--/子类-->
-  <div class="wrapper clearfix">
-    <div class="wrap-box">
-      <ul class="img-list">
-
-        <li>
-          <a href="/goods/show-91.html">
-            <div class="img-box">
-              <img src="/upload/201504/20/thumb_201504200214471783.jpg">
-            </div>
-            <div class="info">
-              <h3>尼康(Nikon)D3300套机（18-55mm f/3.5-5.6G VRII）（黑色）</h3>
-              <p class="price">
-                <b>¥3180.00</b>元</p>
-              <p>
-                <strong>库存 10</strong>
-                <span>市场价：
-                  <s>3150.00</s>
-                </span>
-              </p>
-            </div>
-          </a>
-        </li>
-
-
-
-      </ul>
+        </ul>
+      </div>
     </div>
   </div>
   </div>
@@ -51,19 +44,26 @@
 
 <script>
 export default {
-  data(){
-    return{
+  data() {
+    return {
+      dates: [
 
-    }
+      ]
+
+
+    };
   },
   methods: {
-    getMain(){
-      this.$http.get(this.$api.getgoodsgroup).then(res=>{
-        console.log(res);
-      })
+    getMain() {
+      this.$http.get(this.$api.goodsContent).then(res => {
+        // console.log(res);
+        if (res.data.status == 0) {
+          this.dates = res.data.message;
+        }
+      });
     }
   },
-  created(){
+  created() {
     this.getMain();
   }
 };
